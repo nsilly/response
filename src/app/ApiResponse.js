974ce32eef1f4b6c6aab9a2d35383f9bc8dca0ea
Hieupv <hieupv@codersvn.com>
2018-12-06 00:59:57 +0700

    build with loose option

--- src/app/ApiResponse.js
+++ src/app/ApiResponse.js
@@ -2,10 +2,26 @@ import { LengthAwarePaginator } from './LengthAwarePaginator';
 import { Exception } from '@nsilly/exceptions';
 
 export class ApiResponse {
+  /**
+   * Bind an item to a transformer and start building a response
+   *
+   * @param {*} Object
+   * @param {*} Transformer
+   *
+   * @return Object
+   */
   static item(obj, transformer) {
     return { data: transformer.get(obj) };
   }
 
+  /**
+   * Bind a collection to a transformer and start building a response
+   *
+   * @param {*} collection
+   * @param {*} transformer
+   *
+   * @return Object
+   */
   static collection(collection, transformer) {
     const data = collection.map(i => {
       return transformer.get(i);
@@ -13,6 +29,13 @@ export class ApiResponse {
     return { data: data };
   }
 
+  /**
+   * Build response object for array
+   *
+   * @param {*} array
+   *
+   * @return Object
+   */
   static array(array) {
     if (!Array.isArray(array)) {
       throw new Exception('ApiResponse.array expect an array', 2001);
@@ -20,6 +43,14 @@ export class ApiResponse {
     return { data: array };
   }
 
+  /**
+   * Bind a paginator to a transformer and start building a response
+   *
+   * @param {*} LengthAwarePaginator
+   * @param {*} Transformer
+   *
+   * @return Object
+   */
   static paginate(paginator, transformer) {
     if (!(paginator instanceof LengthAwarePaginator)) {
       throw new Exception(`ApiResponse.paginate expect a LengthAwarePaginator instead a ${typeof paginator}`, 2001);
@@ -42,6 +73,11 @@ export class ApiResponse {
     };
   }
 
+  /**
+   * Build a sucess response
+   *
+   * @return Object
+   */
   static success() {
     return { data: { success: true } };
   }
